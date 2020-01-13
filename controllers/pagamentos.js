40abb89670a634c341a7becc19f417d3f63ae8de
Emmanuel D'Abruzzo <emmanuel.oliveira3@gmail.com>
2020-01-13 18:32:21 -0300

    Altera o controller pagamento para adicionar rota de testes

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,4 +4,13 @@ module.exports = function(app){
 	app.get('/pagamentos', function(req, res){
 		res.send('ok');
 	});
+
+	app.get('/teste', function(req, res){
+  console.log('Recebida requisicao de teste na porta 3000.')
+  res.send('OK.');
+});
+
+
+
+
 }
